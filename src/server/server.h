fe2a545af6b18c093146539e4d4d7643a3293262
napoknot21 <kinbrian5@hotmail.com>
2023-03-12 12:34:57 +0100

    define strut and variables

--- src/server/server.h
+++ src/server/server.h
@@ -1,6 +1,15 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#define BUFF_SIZE 4096
+#define SOCKET_ERROR (-1)
+#define SOCKET_SUCCESS 0
+#define SERVER_BACKLOG 100
+#define SERVER_PORT 7070 //tmp
+
+typedef struct sockaddr_in SA_IN;
+typedef struct sockaddr SA;
+
 void * handle_connection (void *p_client_socket);
 int check (int exp, const char *msg);
 
