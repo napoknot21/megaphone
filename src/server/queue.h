7704cd4e88aaaf1cd315b3c8a1451730484e08f9
napoknot21 <kinbrian5@hotmail.com>
2023-03-12 12:51:56 +0100

    add of tmp queue structure

---
+++ src/server/queue.h
@@ -0,0 +1,15 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+
+struct node {
+    struct node *next;
+    int *client_socket;
+};
+
+typedef struct node node_t;
+
+void enqueue (int *client_socket);
+int *dequeue (void);
+
+#endif
