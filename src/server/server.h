fec62d03c5ed4f16816076a0f6bec6e8b3f241f4
napoknot21 <kinbrian5@hotmail.com>
2023-03-12 14:07:19 +0100

    start of threading handlers

--- src/server/server.h
+++ src/server/server.h
@@ -6,11 +6,18 @@
 #define SOCKET_SUCCESS 0
 #define SERVER_BACKLOG 100
 #define SERVER_PORT 7070 //tmp
+#define THREAD_POOL_SIZE 5
 
 typedef struct sockaddr_in SA_IN;
 typedef struct sockaddr SA;
 
+pthread_t thread_pool[THREAD_POOL_SIZE];
+pthread_mutex_t mutex = PTHREAD_MUTEXT_INITIALIZER;
+//Let's thread wait until something happens and it can do something useful
+pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;  
+
 void * handle_connection (void *p_client_socket);
 int check (int exp, const char *msg);
+void * thread_func (void *arg);
 
 #endif
