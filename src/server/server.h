8390dc6adc5cf5352675e7cfe4250e8e2d45cda6
napoknot21 <kinbrian5@hotmail.com>
2023-03-16 02:43:10 +0100

    server modifications

--- src/server/server.h
+++ src/server/server.h
@@ -2,23 +2,31 @@
 #define SERVER_H
 
 #define BUFF_SIZE 4096
-#define SOCKET_ERROR (-1)
-#define SOCKET_SUCCESS 0
+//#define SOCKET_ERROR (-1)
+//#define SOCKET_SUCCESS 0
 #define SERVER_BACKLOG 100
 #define SERVER_PORT 7070 //tmp
-#define THREAD_POOL_SIZE 5
+//#define THREAD_POOL_SIZE 5
 #define IP_ADDR "127.0.0.1"
 
 typedef struct sockaddr_in SA_IN;
 typedef struct sockaddr SA;
 
-pthread_t thread_pool[THREAD_POOL_SIZE];
-pthread_mutex_t mutex = PTHREAD_MUTEXT_INITIALIZER;
+//pthread_t thread_pool[THREAD_POOL_SIZE];
+//pthread_mutex_t mutex = PTHREAD_MUTEXT_INITIALIZER;
 //Let's thread wait until something happens and it can do something useful
-pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;  
+//pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;  
 
-void * handle_connection (void *p_client_socket);
-int check (int exp, const char *msg);
-void * thread_func (void *arg);
+
+void handle_signal (int sig);
+void check_socket (int *serv_socket);
+void check_bind (int *serv_socket, SA_IN *serv_addr);
+void check_listen (int *serv_socket, int nb_connections);
+void check_accept (int *serv_socket, int *cli_socket, SA *cli_addr);
+
+
+//void * handle_connection (void *p_client_socket);
+//int check (int exp, const char *msg);
+//void * thread_func (void *arg);
 
 #endif
