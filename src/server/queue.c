5f04fce9ae79e3d03341f3c56f1eccafc876aa3a
napoknot21 <kinbrian5@hotmail.com>
2023-03-16 02:26:34 +0100

    queue structure improves

--- src/server/queue.c
+++ src/server/queue.c
@@ -1,45 +1,87 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "queue.h"
 
-node_t *head = NULL;
-node_t *tail = NULL;
+struct node *head = NULL;
+struct node *current = NULL;
+
+int isEmpty () 
+{
+    return head == NULL;
+}
+
+
+struct node * createNode (int *client_socket) 
+{
+    struct node *new_node = (struct node *) malloc (sizeof(struct node));
+    new_node->client_socket = client_socket;
+    new_node->next = NULL;
+    return new_node;
+}
+
+
+struct node * createQueue (int *client_socket)
+{
+    head = createNode(client_socket);
+    return head;
+}
+
 
 void enqueue (int *client_socket) 
 {
-    node_t *new_node = malloc (sizeof(node_t));
+    struct node *new_node = (struct node *) malloc (sizeof(struct node));
+
     new_node->client_socket = client_socket;
-    new_node->next = NULL;
-    if (tail == NULL) {
-        head = new_node;
-    } else {
-        tail->next = new_node;
-    }
-    tail = new_node;
+    new_node->next = head;
+    
+    head = new_node;
 }
 
-int *dequeue () 
+
+void deleteNode (struct node *to_delete)
 {
-    if (head == NULL) {
-        return NULL;
+    if (to_delete == NULL) return;
+
+    if (to_delete == head) {
+        if (to_delete->next == NULL) {
+            free(head);
+        } else {
+            current = head;
+            head = head->next;
+            free(current);
+        }
+        return;
+    } 
+    
+    current = head;
+    while (current->next != to_delete && current->next != NULL) {
+        current = current->next;
     }
-    int *result = head->client_socket;
-    node_t *tmp = head;
-    head = head->next;
-    if (head == NULL) tail = NULL;
-    free(tmp);
-    return result
+    
+    if (current->next == NULL) {
+        return;
+    }
+
+    currrent->next = to_delete->next;
+    free(to_delete);
+}
+
+
+void dequeue () 
+{
+    deleteNode(head);
+}
+
+
+void freeNode (struct node *to_delete)
+{
+    free(to_delete->client_socket);
+    free(to_delete);
 }
 
 
 void freeQueue ()
 {
-    node_t *tmp;
-
-    while (head != NULL) {
-        tmp = head;
-        head = head->next;
-        free(tmp)
-    }
 }
