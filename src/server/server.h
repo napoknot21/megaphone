e0c295955d0badceb7179bc13faf16090cd5551a
napoknot21 <kinbrian5@hotmail.com>
2023-03-19 15:19:24 +0100

    changes in server.h

--- src/server/server.h
+++ src/server/server.h
@@ -1,33 +1,28 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#define STATUS_ERROR (-1)
+#define STATUS_SUCCESS 0
+
 #define BUFF_SIZE 4096
-//#define SOCKET_ERROR (-1)
-//#define SOCKET_SUCCESS 0
-#define SERVER_BACKLOG 100
+#define MAX_CLIENTS  10
+
 #define SERVER_PORT 7070 //tmp
-//#define THREAD_POOL_SIZE 5
 #define LOCAL_ADDR4 "127.0.0.1"
 #define LOCAL_ADDR6 "::1"
 
-//typedef struct sockaddr_in SA_IN;
-//typedef struct sockaddr SA;
-
 //pthread_t thread_pool[THREAD_POOL_SIZE];
 //pthread_mutex_t mutex = PTHREAD_MUTEXT_INITIALIZER;
-//Let's thread wait until something happens and it can do something useful
 //pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;  
 
 int load_server (void);
+void * handle_client (void *p_client_socket);
 void handle_signal (int sig);
+
+// Check functions
 void check_socket (int *serv_socket);
 void check_bind (int *serv_socket, (struct sockaddr_in) *serv_addr);
 void check_listen (int *serv_socket, int nb_connections);
 void check_accept (int *serv_socket, int *cli_socket, SA *cli_addr);
-
-
-//void * handle_connection (void *p_client_socket);
-//int check (int exp, const char *msg);
-//void * thread_func (void *arg);
 
 #endif
